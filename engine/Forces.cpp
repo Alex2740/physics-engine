21efc3728bf12e6bc246b6dc73b840afe2b2982d
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2022-09-24 10:07:17 -0400

    add Force.cpp to calculate forces

---
+++ engine/Forces.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include "Forces.h"
+
+Vector3* Forces::gravitation(Particule* src, Particule* dst) {
+    if (src == dst) return nullptr;
+
+    float dist_x = src->position->x - dst->position->x;
+    float dist_y = src->position->y - dst->position->y;
+    float dist_z = src->position->z - dst->position->z;
+
+    float dist = sqrtf(powf(dist_x, 2) + powf(dist_y, 2) + powf(dist_z, 2));
+
+    if (dist < 0.01) dist = 0.01;
+
+    float uni_x = dist_x / dist;
+    float uni_y = dist_y / dist;
+    float uni_z = dist_z / dist;
+
+
+    float magnitude = Forces::GRAVITATION_CONST * src->masse() * dst->masse() / powf(dist, 2);
+
+    return new Vector3(magnitude * uni_x, magnitude * uni_y, magnitude * uni_z);
+}
---
+++ engine/Forces.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Vector3.h"
+#include "Particule.h"
+
+namespace Forces {
+    const float GRAVITATION_CONST = 0.001f;
+    Vector3* gravitation(Particule* p1, Particule* p2);
+}
+
