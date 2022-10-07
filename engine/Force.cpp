faadeed508cb10dbae6abe91dd7663a132421382
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2022-10-07 00:07:25 -0400

    add the new registry implementation WARNING: not tested

--- engine/Force.cpp
+++ engine/Force.cpp
@@ -15,3 +15,47 @@ Force::Gravity::Gravity(Particule * p) {
 Vector3 Force::Gravity::getForce() {
     return Vector3(0, this->val, 0);
 }
+
+Registry::ParticuleRegistry::ParticuleRegistry(Particule* p) {
+    this->particule = p;
+}
+
+std::vector<Force::Force> Registry::ParticuleRegistry::getRegist() {
+    return this->forceRegistry;
+}
+
+void Registry::ParticuleRegistry::addForce(Force::Force f) {
+    this->forceRegistry.push_back(f);
+}
+
+bool Registry::ParticuleRegistry::delForce(Force::Force f) {
+    std::remove(this->forceRegistry.begin(), this->forceRegistry.end(), f);
+}
+
+void Registry::ParticuleRegistry::update(float dt) {
+    Vector3 sum = Vector3::Zero();
+    for (auto f: this->forceRegistry) {
+        sum += f.getForce();
+    }
+    this->particule->integrate(sum, dt);
+}
+
+bool Registry::ParticuleRegistries::addParticule(Particule* p) {
+    if (this->registries.count(p) == 0) return false;
+    // TODO: not sure if this works
+    // probably not
+    this->registries.emplace(p, p);
+    return true;
+}
+
+bool Registry::ParticuleRegistries::addForce(Particule* p, Force::Force f) {
+    if (this->registries.count(p) == 0) return false;
+    this->registries[p].addForce(f);
+    return true;
+}
+
+void Registry::ParticuleRegistries::addForceAll(Force::Force f) {
+    for (auto p: this->registries) {
+        addForce(p.first, f);
+    }
+}
--- engine/Force.h
+++ engine/Force.h
@@ -2,9 +2,11 @@
 
 #pragma once
 
+#include <map>
+
 #include "Vector3.h"
 #include "Particule.h"
-
+// ############ FORCE ###########
 namespace Force
 {
     const float GRAVITY_CONST = 9.81f;
@@ -31,3 +33,43 @@ namespace Force
         Vector3 getForce() override;
     };
 }
+// ######### REGISTRY #########
+namespace Registry
+{
+    class ParticuleRegistry
+    {
+        // registry for a single particle
+        // containing a list of Force
+        private:
+        Particule* particule;
+        std::vector<Force::Force> forceRegistry;
+        // reserved place for future registries
+
+        public:
+        ParticuleRegistry(Particule* p);
+        ~ParticuleRegistry();
+        void addForce(Force::Force f);
+        bool delForce(Force::Force f);
+
+        std::vector<Force::Force> getRegist(void);
+
+        void update(float dt);
+    };
+
+    class ParticuleRegistries
+    {
+        private:
+        std::map<Particule*, ParticuleRegistry> registries;
+
+        public:
+        // add a new item in the registres
+        bool addParticule(Particule* p);
+        bool delParticule(Particule* p);
+
+        bool addForce(Particule* p, Force::Force f);
+        void addForceAll(Force::Force f);
+
+        void updateAll(float dt); // update all the particles
+    };
+}
+
