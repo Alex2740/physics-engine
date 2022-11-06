f0c3263d4829396e17270e82e4c3f57f830b3e14
BOUGUET Theo <theo.bougue@gmail.com>
2022-11-05 20:28:43 -0400

    Ajout définition rigidbody

---
+++ engine/Rigidbody.cpp
@@ -0,0 +1,25 @@
+#include "Rigidbody.h"
+
+void RigidBody::Intergrate(float dt)
+{
+}
+
+void RigidBody::AddForce(const Vector3& force)
+{
+}
+
+void RigidBody::AddForceAtPoint(const Vector3& force, const Vector3& worldPoint)
+{
+}
+
+void RigidBody::AddForceAtBodyPoint(const Vector3& force, const Vector3& localPoint)
+{
+}
+
+void RigidBody::ClearAccumulator()
+{
+}
+
+void RigidBody::CalculateDerivedData()
+{
+}
---
+++ engine/Rigidbody.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Vector3.h"
+#include "Quaternion.h"
+#include "Matrix4.h"
+#include "Matrix3.h"
+
+class RigidBody {
+
+private : 
+	float inverseMasse;
+	float linearDamping;
+	Vector3 position;
+	Vector3 velocity;
+
+	Quaternion orientation;
+	Vector3 rotation;
+	Matrix4 transformMatrix;
+
+	float angularDamping;
+	Vector3 forceAccum;
+	Vector3 torqueAccum;
+
+public : 
+	
+	void Intergrate(float dt);
+
+	void AddForce(const Vector3& force);
+
+	void AddForceAtPoint(const Vector3& force, const Vector3& worldPoint);
+
+	void AddForceAtBodyPoint(const Vector3& force, const Vector3& localPoint);
+
+	void ClearAccumulator();
+
+private :
+
+	void CalculateDerivedData();
+
+
+};
