809ce49684b4506503e52c2df6b6e1ace11e5379
Valentin PW <Valentin.Plantard--Wahl@telecomnancy.eu>
2022-11-20 11:19:54 -0500

    More Cubes !!!!

--- main.cpp
+++ main.cpp
@@ -76,6 +76,19 @@ GLuint indices[] =
 	3, 0, 4
 };
 
+glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 int main()
 {
 	// Initialize GLFW
@@ -113,6 +126,10 @@ int main()
 
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("resources/shaders/default.vert", "resources/shaders/default.frag");
+
+	glm::mat4 model = glm::mat4(1.0f);
+	int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 	// Generates Vertex Array Object and binds it
 	VAO VAO1;
@@ -167,6 +184,17 @@ int main()
 
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
+
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, &model[0][0]);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawArrays(GL_TRIANGLES, 0, 36);
