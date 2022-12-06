d9016607a7b95719cb238b4b01a6211c7af576b5
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2022-12-06 18:28:21 -0500

    add a new implementation

--- src/physics/contact/CollisionDetector.cpp
+++ src/physics/contact/CollisionDetector.cpp
@@ -5,7 +5,7 @@ int CollisionDetector::SphereAndSphere(Sphere& one, Sphere& two, CollisionData* 
 	// Teste s'il reste de la place dans le tableau des contacts
 	if (data->contactsLeft <= 0) return 1;
 
-	// R�cup�ration des positions des spheres
+	// Récupération des positions des spheres
 	Vector3 positionSphere1 = one.offset * one.body->position;
 	Vector3 positionSphere2 = two.offset * two.body->position;
 
@@ -32,10 +32,40 @@ int CollisionDetector::SphereAndSphere(Sphere& one, Sphere& two, CollisionData* 
 	return 1;
 }
 
+int BoxAndPoint(Box& one, Vector3& two, CollisionData* data) {
+	// first step: if it is outside the box,
+	// 			   then surely no collision
+	if (two.getMagnitude() > one.halfSize.getMagnitude()) {
+		return 0;
+	}
+	// second step : do the projection on each plain of the cube,
+	// see if the point fall into the projection range
+	// to do this, we use the coordinate of cube, move the point
+	// into this coordinate
+	Matrix4 inverseTransformMat = one.offset.inverse();
+	Vector3 transformedPoint = inverseTransformMat * two;
+	if (!(std::abs(transformedPoint.x) < std::abs(one.halfSize.x)
+	&&  std::abs(transformedPoint.y) < std::abs(one.halfSize.y)
+	&&  std::abs(transformedPoint.z) < std::abs(one.halfSize.z)
+	)) {
+		// no collision if it is not in all the three projections
+		return 0;
+	}
+	Contact contact;
+	contact.body[0] = one.body;
+	contact.body[1] = nullptr;
+	// TODO: check other data
+	contact.contactNormal = Vector3::Zero();
+
+	data->AddContact(contact);
+	return 1;
+}
+
 int BoxAndPlane(Box& one, Plane& two, CollisionData* data) {
 	// first step: get all the verties, so that I can test if any pair of vertices is crossing the plane "two"
 	std::array<Vector3, 8> vertices = one.getLocalCoordVertices();
 	std::vector<float> distances = std::vector<float>();
+	// second step: for each vertex found, I can calculate their relative distance to the plain. The sign indicate on which side the vertex is found.
 	for(Vector3 v: vertices) {
 		distances.push_back(two.getDistanceToPoint(v));
 	}
@@ -44,6 +74,8 @@ int BoxAndPlane(Box& one, Plane& two, CollisionData* data) {
 		// every vertex at the same side; no collision
 		return 0;
 	}
+	// if there are two vertices on the different side, then collision
+
 	// float penetration;
 	// if (std::abs(distances.at(0)) > std::abs(distances.at(7))) {
 	// 	penetration = -distances.at(0);
@@ -62,6 +94,7 @@ int BoxAndPlane(Box& one, Plane& two, CollisionData* data) {
 	contact.contactNormal = two.normal;
 	contact.penetration = std::max(std::abs(distances.at(0)), std::abs(distances.at(7)));
 	Vector3 cubeProjectionOnPlane = two.normal * two.offset + (one.body->position - Vector3::Cross(one.body->position, two.normal));
+	// TODO: this needs some discussions
 	contact.contactPoint = cubeProjectionOnPlane;
 	contact.friction; //TODO
 	contact.restitution; // TODO
